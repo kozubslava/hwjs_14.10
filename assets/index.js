7433e2b73d94a140a0c79f27098f9df4bc72aee9
Вячеслав <146669463+kozubslava@users.noreply.github.com>
2023-11-19 17:34:13 +0200

    add bonus function

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 
 
+// 1. Створити функцію, яка буде знаходити найменьше з двох чисел
 const value1 = 5;
 const value2 = 4;
 
@@ -15,7 +16,7 @@ console.log("lessValue ", lessValue(value1,value2));
 
 
 
-
+// 2.Створити функцію, яка буде отимувати вік людини і казати, повнолітня вона, чи ні
 const age = 5
 const validAge = function(value){
 if (value >= 18) {
@@ -26,6 +27,7 @@ if (value >= 18) {
 
 console.log(validAge(age));
 
+// 3. Створити функцію - калькулятор, яка буде приймати два числа та оператор (+, -, /, *) і буде повертати результат вказаної операції над числами. Зробити варіант або на if...else або на switch...case
 
 const calc = function (a,b, symbol) {
   if (typeof a !== "number" || typeof b !== "number" || a===0 || b===0  ) {
@@ -51,6 +53,35 @@ console.log(calc(3,"t","/"));
 console.log(calc(3,0,"/"));
 
 
+// 4. Bonus
+const calcBonus = function (a,b, symbol){
+  if (typeof a !== "number" || typeof b !== "number" || a===0 || b===0 ){
+    return "error"
+  }
+  switch (symbol) {
+    case "+": {
+      return (a + b);
+    }
+    case "-": {
+      return (a - b);
+    }
+    case "*": {
+      return (a * b);
+    }
+    case "/": {
+      return (a / b);
+    }
+    default: return "unknown symbol"
+      break;
+  }
+}
+console.log(calcBonus(3,2,"+"));
+console.log(calcBonus(3,2,"-"));
+console.log(calcBonus(3,2,"*"));
+console.log(calcBonus(3,2,"/"));
+console.log(calcBonus(3,2,"&"));
+console.log(calcBonus(3,"t","/"));
+console.log(calcBonus(3,0,"/"));
 
 
 
